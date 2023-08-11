7ecbe925813e2b4275dce23682cce3be5ade5142
iyadxxadjeroud <iyadadjeroud10@gmail.com>
2023-08-11 01:31:51 +0100

    DONE

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,10 +2,10 @@
 /**
  * main - entry point
  *
- *return value of main must be always 0 (success)
+ *Return value of main must be always 0 (success)
  */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return (0); /* this is values of return 'main' */
+	return (0); 
 }
