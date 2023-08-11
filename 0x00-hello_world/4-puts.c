23e058a3386644de88a7f7c4275e9aa056dfca0a
iyadxxadjeroud <iyadadjeroud10@gmail.com>
2023-08-11 01:19:14 +0100

    DONE

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+/**
+ *This is code for print a message without using printf function
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
