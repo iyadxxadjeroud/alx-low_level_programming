abff6fdb502489f2ee6f070b2de3a8de236ffef0
iyadxxadjeroud <iyadadjeroud10@gmail.com>
2023-08-15 20:18:09 +0100

    The alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ *main - Entry point
+ *Description: prints the alphabet in lowercase
+ *
+ *Return: always 0 (Success)
+ */
+void print_alphabet(void);
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		_putchar(ch);
+	_putchar('\n');
+}
