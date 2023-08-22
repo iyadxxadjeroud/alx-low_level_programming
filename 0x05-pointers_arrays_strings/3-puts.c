8915896d1752d90c983f3dafcef54cddb46c5440
iyadxxadjeroud <iyadadjeroud10@gmail.com>
2023-08-22 03:22:34 +0100

    DONE

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -7,10 +7,16 @@
  */
 void _puts(char *str)
 {
-	while (*str != '\0')
-	{{
-		_putchar(*str + 0);
-		str++;
+	int count = 0;
+
+	while (count >= 0)
+	{
+		if (str[count] == '\0')
+		{
+			_putchar('\n');
+			break;
+		}
+		_putchar(str[count]);
+		count++;
 	}
-	_putchar('\n');
 }
