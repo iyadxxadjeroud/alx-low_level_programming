0154771d9863359d96208ed36fadd80355878ba7
iyadxxadjeroud <iyadadjeroud10@gmail.com>
2023-09-13 01:23:46 +0100

    DONE

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - function that prints the name of the file it was compiled from,
+ *			followed by a new line.
+ * Return: always 0 success
+ */
+
+int main(void)
+{
+	__FILE__;
+	return (0);
+}
