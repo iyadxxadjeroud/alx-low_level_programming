44d8f2678b06b61329cd20b371f57eac5a5fa74d
iyadxxadjeroud <iyadadjeroud10@gmail.com>
2023-12-24 02:47:40 +0100

    DONE

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,21 +9,21 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *h_table;
-	unsigned long int i;
 
-	h_table = malloc(sizeof(hash_table_t));
+	if (size == 0)
+		return (NULL);
+
+	h_table = calloc(1, sizeof(hash_table_t));
 	if (h_table == NULL)
 	{
 		return (NULL);
 	}
 	h_table->size = size;
-	h_table->array = malloc(size * sizeof(hash_node_t *));
+	h_table->array = calloc(size, sizeof(hash_node_t *));
 	if (h_table->array == NULL)
 	{
 		free(h_table);
 		return (NULL);
 	}
-	for (i = 0; i < size; i++)
-		h_table->array[i] = NULL;
 	return (h_table);
 }
